763781dda4fe2e0e101e159dea04d8dffc70f13d
ShaiSundar511 <78576639+ShaiSundar511@users.noreply.github.com>
2022-04-08 11:58:06 -0400

    Update app.py
    reverting back

--- app.py
+++ app.py
@@ -23,52 +23,29 @@ server = app.server
 # Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
 server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
-df_country = pd.read_csv("https://raw.githubusercontent.com/smbillah/ist526/main/gapminder.csv")
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-my_fig = px.bar(
-  data_frame = df_country, 
-  y="pop",         # gdp per capita
-  x="continent",           # life expectancy  
-  #size="pop",            # population
-  color="country",     # group/label
-  hover_name="country",
-  #log_x=True, 
-  #size_max=55, 
-  #range_x=[100,100000], 
-  #range_y=[25,90],
-  title= "GDP Per Captia vs Life Expectancy of Countries", 
-  
-  # animation control
-  #animation_frame="year", 
-  #animation_group="country",
-)
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div([
-  # first row: header
-  html.H1('Shai Sundar\'s Custom Dashboard'),
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),#'<h1> this is a header </h1>'
+    #
+    # html.Div(children='''
+    #     Dash: A web application framework for Python.
+    # '''),
 
-  # second row: <scratter-plot> <empty> <bar chart> 
-
-    # bar chart
-    html.Div([
-        dcc.Dropdown(),
-
-        dcc.Graph(
-          id='my_fig', 
-          figure=my_fig
-        )
-    ], className='six columns')
-    
-  ], className = 'row'),
-
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
-
-  
-# run the code
-# uncomment the following line to run in Google Colab
-app.run_server(mode='inline', port=8030)
 
 # Run dash app
 if __name__ == "__main__":
     app.run_server(debug=False, host='0.0.0.0', port=8050)
- 
